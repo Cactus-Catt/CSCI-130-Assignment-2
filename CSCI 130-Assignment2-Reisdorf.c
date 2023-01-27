3b6e846074ead4495624512962d0ef08a9eebec5
Courtney Reisdorf <122465222+Cactus-Catt@users.noreply.github.com>
2023-01-26 21:18:39 -0600

    Add files via upload

---
+++ CSCI 130-Assignment2-Reisdorf.c
@@ -0,0 +1,66 @@
+// Assignment 2 - Reisdorf - CSCI 130, Online, Spring 2023 //
+// Program arithmetic and comparing three numbers in C //
+
+#include <stdio.h>
+
+int main(void) {
+
+int x, y, z, sum, average, product;
+  
+printf("Enter three different numbers: ");
+scanf("%d%d%d", &x, &y, &z);
+
+sum = x + y + z;
+
+  printf("The Sum is: %d\n", sum);
+  
+average = ( x + y + z ) / 3;
+
+  printf("The Average is: %d\n", average);
+  
+product = ( x * y ) * z;
+
+  printf("The Product is : %d\n", product);
+
+  //if statement - "smallest number is:" //
+
+if ( x < y ) {
+    if ( x < z ) {
+      printf("The smallest number is: %d\n", x );
+    }
+  }
+
+if ( y < x ) {
+  if (y < z ) {
+    printf("The smallest number is: %d\n", y );
+  }
+}  
+
+if ( z < x ) {
+  if ( z < y ) {
+    printf("The smallest number is: %d\n", z);
+  }
+} 
+
+// "largest number is:" //
+
+if ( x > y ) {
+  if ( x > z ) {
+    printf("The largest number is: %d\n", x);
+  }
+} 
+
+if ( y > x ) {
+  if ( y > z ) {
+    printf("The largest number is: %d\n", y);
+  }
+} 
+
+if ( z > x ) {
+  if ( z > y ) {
+    printf("The largest number is: %d\n", z);
+  }
+} 
+  
+  return 0;
+}
